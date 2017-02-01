72c33386301f69fee18052ad0a4b85bafd0f79d0
BeetoGuy <BeetoGuy@gmail.com>
2017-02-01 14:39:17 -0700

    Fix Hardcore Buckets overriding water sources
    Fixes #216

--- src/main/java/betterwithmods/BWMod.java
+++ src/main/java/betterwithmods/BWMod.java
@@ -46,7 +46,7 @@ import java.util.*;
 @Mod(modid = BWMod.MODID, name = BWMod.NAME, version = BWMod.VERSION, dependencies = "required-after:Forge@[12.18.1.2076,);before:survivalist;after:tconstruct;after:minechem;after:natura;after:terrafirmacraft;after:immersiveengineering", guiFactory = "betterwithmods.client.gui.BWGuiFactory")
 public class BWMod {
     public static final String MODID = "betterwithmods";
-    public static final String VERSION = "0.14.5 Beta";
+    public static final String VERSION = "0.14.7 Beta";
     public static final String NAME = "Better With Mods";
     private static final Set<ICompatModule> loadedModules = new HashSet<>();
 
