01467b9b6b7a7ec15c0e3993969e9f572429a108
BeetoGuy <BeetoGuy@gmail.com>
2017-08-25 13:38:56 -0700

    Fix #541

--- src/main/java/betterwithmods/client/container/bulk/GuiMill.java
+++ src/main/java/betterwithmods/client/container/bulk/GuiMill.java
@@ -21,6 +21,7 @@ public class GuiMill extends GuiContainer {
 
     @Override
     public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+        this.drawDefaultBackground();
         super.drawScreen(mouseX, mouseY, partialTicks);
         this.renderHoveredToolTip(mouseX, mouseY);
     }
