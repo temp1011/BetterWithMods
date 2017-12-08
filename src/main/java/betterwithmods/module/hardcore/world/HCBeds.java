fbb2e07941b59836313749f8d5bac0cc4bf1ade9
primetoxinz <primetoxinzz@gmail.com>
2017-12-07 23:26:40 -0500

    I don't wanna but I guess... Closes #633

--- src/main/java/betterwithmods/module/hardcore/world/HCBeds.java
+++ src/main/java/betterwithmods/module/hardcore/world/HCBeds.java
@@ -14,6 +14,13 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public class HCBeds extends Feature {
     public static final EntityPlayer.SleepResult TOO_RESTLESS = EnumHelper.addEnum(EntityPlayer.SleepResult.class, "TOO_RESTLESS", new Class[0]);
 
+    public static boolean stillSetSpawn;
+
+    @Override
+    public void setupConfig() {
+        stillSetSpawn = loadPropBool("Still Set Spawn", ":( I guess I'll be nice and give this config", false);
+    }
+
     @Override
     public String getFeatureDescription() {
         return "Disables the ability to sleep in a bed and can no longer set spawn";
@@ -27,6 +34,9 @@ public class HCBeds extends Feature {
         if (PlayerHelper.isSurvival(event.getEntityPlayer())) {
             event.getEntityPlayer().sendStatusMessage(new TextComponentTranslation("tile.bed.tooRestless"), true);
             event.setResult(TOO_RESTLESS);
+            if(stillSetSpawn) {
+                event.getEntityPlayer().setSpawnPoint(event.getPos(),true);
+            }
         }
     }
 
