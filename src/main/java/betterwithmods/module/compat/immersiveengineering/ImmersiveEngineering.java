657f322ed80a0e2a09b2dc92034161dd22d6438c
mmelvin0 <mmelvin0@gmail.com>
2017-12-21 12:30:06 -0800

    Fix IE hemp seeds not being blacklisted from hoe drops (#706)

--- src/main/java/betterwithmods/module/compat/immersiveengineering/ImmersiveEngineering.java
+++ src/main/java/betterwithmods/module/compat/immersiveengineering/ImmersiveEngineering.java
@@ -4,7 +4,7 @@ import betterwithmods.module.CompatFeature;
 import betterwithmods.module.hardcore.needs.HCSeeds;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 /**
@@ -21,7 +21,7 @@ public class ImmersiveEngineering extends CompatFeature {
     public static final Item HEMP_SEED = null;
 
     @Override
-    public void preInit(FMLPreInitializationEvent event) {
+    public void init(FMLInitializationEvent event) {
         HCSeeds.SEED_BLACKLIST.add(new ItemStack(HEMP_SEED));
     }
 }
