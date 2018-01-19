769acf7b5fadcd025cceb7f9171451f2f297abfa
primetoxinz <primetoxinzz@gmail.com>
2018-01-19 13:12:42 -0500

    Work on adding more to improved flee. Group flee needs more work.

--- src/main/java/betterwithmods/module/tweaks/ImprovedFlee.java
+++ src/main/java/betterwithmods/module/tweaks/ImprovedFlee.java
@@ -12,6 +12,7 @@ import net.minecraft.entity.passive.EntityCow;
 import net.minecraft.entity.passive.EntityTameable;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.event.entity.living.LivingSetAttackTargetEvent;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
@@ -20,13 +21,14 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
  */
 public class ImprovedFlee extends Feature {
 
-    private static boolean blockPlace;
+    private static boolean blockPlace, blockBreak, groupFlee;
 
 
     @Override
     public void setupConfig() {
         blockPlace = loadPropBool("Animals Flee from Block Place", "When a block is placed in the vicinity of an animal it will get scared and run.", true);
-
+        blockBreak = loadPropBool("Animals Flee from Block Break", "When a non-replaceable block is broken in the vicinity of an animal it will get scared and run.", true);
+//        groupFlee = loadPropBool("Animals Group Flee", "When an animal is attacked in the vicinity of another animal it will get scared and run.", true);
     }
 
     @SubscribeEvent
@@ -48,17 +50,43 @@ public class ImprovedFlee extends Feature {
 
     @SubscribeEvent
     public void onPlaceBlock(BlockEvent.PlaceEvent event) {
-        if(!blockPlace)
+        if (!blockPlace)
             return;
-        if(event.getPlayer() != null) {
-            AxisAlignedBB box = event.getPlacedBlock().getBoundingBox(event.getWorld(),event.getPos()).offset(event.getPos()).grow(10);
-            for(EntityAnimal animal:event.getWorld().getEntitiesWithinAABB(EntityAnimal.class, box)) {
+        if (event.getPlayer() != null) {
+            AxisAlignedBB box = event.getPlacedBlock().getBoundingBox(event.getWorld(), event.getPos()).offset(event.getPos()).grow(10);
+            for (EntityAnimal animal : event.getWorld().getEntitiesWithinAABB(EntityAnimal.class, box)) {
                 animal.setRevengeTarget(event.getPlayer());
             }
         }
     }
 
+    @SubscribeEvent
+    public void onPlaceBreak(BlockEvent.BreakEvent event) {
+        if (!blockBreak)
+            return;
+        if (event.getPlayer() != null && !event.getState().getMaterial().isReplaceable()) {
+            AxisAlignedBB box = event.getState().getBoundingBox(event.getWorld(), event.getPos()).offset(event.getPos()).grow(10);
+            for (EntityAnimal animal : event.getWorld().getEntitiesWithinAABB(EntityAnimal.class, box)) {
+                animal.setRevengeTarget(event.getPlayer());
+            }
+        }
+    }
 
+    @SubscribeEvent
+    public void onGroupFlee(LivingSetAttackTargetEvent event) {
+        if (!groupFlee)
+            return;
+        if (event.getEntityLiving() instanceof EntityAnimal) {
+
+            EntityAnimal a = (EntityAnimal) event.getEntityLiving();
+            AxisAlignedBB box = new AxisAlignedBB(a.posX, a.posY, a.posZ, a.posX + 1, a.posY + 1, a.posZ + 1).grow(10);
+            for (EntityAnimal animal : a.getEntityWorld().getEntitiesWithinAABB(EntityAnimal.class, box, entity ->
+                    entity != null && entity != a && entity.getRevengeTarget() == null)) {
+                System.out.println(animal.getRevengeTimer());
+                animal.setRevengeTarget(event.getTarget());
+            }
+        }
+    }
 
     @Override
     public String getFeatureDescription() {
