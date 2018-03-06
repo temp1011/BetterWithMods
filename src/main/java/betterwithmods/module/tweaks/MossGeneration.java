2f876a6fc6912c2062f65066c73a595e9652c68b
primetoxinz <primetoxinzz@gmail.com>
2018-03-06 18:41:52 -0500

    Possibly stop Moss Generation from loading chunks.

--- src/main/java/betterwithmods/module/tweaks/MossGeneration.java
+++ src/main/java/betterwithmods/module/tweaks/MossGeneration.java
@@ -7,41 +7,62 @@ import net.minecraft.init.Blocks;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.tileentity.TileEntityMobSpawner;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.fml.relauncher.Side;
+import org.apache.commons.lang3.RandomUtils;
 
 import java.util.ConcurrentModificationException;
 import java.util.List;
-import java.util.Random;
 import java.util.stream.Collectors;
 
 /**
  * Created by primetoxinz on 4/20/17.
  */
 public class MossGeneration extends Feature {
+    private static final int RADIUS = 5;
 
     @SubscribeEvent
     public void generateMossNearSpawner(TickEvent.WorldTickEvent event) {
-        if (event.world.isRemote || event.phase != TickEvent.Phase.END || event.side != Side.SERVER)
+        World world = event.world;
+        if (world.isRemote || event.phase != TickEvent.Phase.END || event.side != Side.SERVER)
             return;
-        Random rand = event.world.rand;
         try {
-	        List<BlockPos> positions = event.world.loadedTileEntityList.stream().filter(t -> t instanceof TileEntityMobSpawner).map(TileEntity::getPos).collect(Collectors.toList());
+            List<BlockPos> positions = world.loadedTileEntityList.stream().filter(t -> t instanceof TileEntityMobSpawner).map(TileEntity::getPos).collect(Collectors.toList());
             positions.forEach(pos -> {
-                int x = rand.nextInt(9) - 4;
-                int y = rand.nextInt(5) - 1;
-                int z = rand.nextInt(9) - 4;
-                BlockPos check = pos.add(x, y, z);
-                IBlockState state = event.world.getBlockState(check);
-                if ((state.getBlock() == Blocks.COBBLESTONE || (state.getBlock() == Blocks.STONEBRICK && state.getBlock().getMetaFromState(state) == 0)) && rand.nextInt(30) == 0) {
-                    IBlockState changeState = state.getBlock() == Blocks.COBBLESTONE ? Blocks.MOSSY_COBBLESTONE.getDefaultState() : Blocks.STONEBRICK.getDefaultState().withProperty(BlockStoneBrick.VARIANT, BlockStoneBrick.EnumType.MOSSY);
-                    event.world.setBlockState(check, changeState);
+                BlockPos min = pos.add(-RADIUS, -RADIUS, -RADIUS), max = pos.add(RADIUS, RADIUS, RADIUS);
+                BlockPos set = randomPosition(world,min,max);
+                if(set != null) {
+                    mossify(world,set);
                 }
             });
         } catch (ConcurrentModificationException ignored) {
-        	ignored.printStackTrace();
+            ignored.printStackTrace();
         }
+    }
+
+    public static BlockPos randomPosition(World world, BlockPos start, BlockPos end) {
+        if(world.isAreaLoaded(start,end)) {
+            return new BlockPos(RandomUtils.nextInt(start.getX(), end.getX()), RandomUtils.nextInt(start.getY(), end.getY()), RandomUtils.nextInt(start.getZ(), end.getZ()));
+        }
+        return null;
+    }
+
+    public static void mossify(World world, BlockPos pos) {
+        IBlockState mossy;
+        if (world.rand.nextInt(30) == 0 && (mossy = getMossyVariant(world.getBlockState(pos))) != null) {
+            world.setBlockState(pos, mossy);
+        }
+    }
+
+    public static IBlockState getMossyVariant(IBlockState state) {
+        if (state.getBlock() == Blocks.COBBLESTONE) {
+            return Blocks.MOSSY_COBBLESTONE.getDefaultState();
+        } else if (state.getBlock() == Blocks.STONEBRICK) {
+            return Blocks.STONEBRICK.getDefaultState().withProperty(BlockStoneBrick.VARIANT, BlockStoneBrick.EnumType.MOSSY);
+        }
+        return null;
     }
 
     @Override
