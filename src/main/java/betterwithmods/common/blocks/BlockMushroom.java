35b4af8b7d1679e91606207e67be15900fc866dc
BordListian <bordlistian@hotmail.de>
2018-03-26 00:41:27 +0200

    - Improves Mushroom Farming Tweak - Aliases Mushrooms (configurable) - Removes the bad config to delete mushrooms if they can't grow in a spot. - Changed the catchall handler to check for mycellium and podzol before dropping the mushroom. - Closes #808

---
+++ src/main/java/betterwithmods/common/blocks/BlockMushroom.java
@@ -0,0 +1,43 @@
+package betterwithmods.common.blocks;
+
+import betterwithmods.module.tweaks.MushroomFarming;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+import java.util.Random;
+
+public class BlockMushroom extends net.minecraft.block.BlockMushroom {
+    public BlockMushroom() {
+        super();
+        setHardness(0.0F);
+        setSoundType(SoundType.PLANT);
+        setUnlocalizedName("mushroom");
+    }
+
+    @Override
+    public void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand) {
+        if(worldIn.getLight(pos) < MushroomFarming.MAX_LIGHT_LEVEL)
+            super.updateTick(worldIn, pos, state, rand);
+    }
+
+    @Override
+    public boolean canBlockStay(World worldIn, BlockPos pos, IBlockState state)
+    {
+        if (pos.getY() >= 0 && pos.getY() < 256)
+        {
+            IBlockState soil = worldIn.getBlockState(pos.down());
+
+            if(MushroomFarming.isMushroomSoil(soil))
+                return true;
+            else
+                return worldIn.getLight(pos) < MushroomFarming.MAX_LIGHT_LEVEL && soil.getBlock().canSustainPlant(soil, worldIn, pos.down(), EnumFacing.UP, this);
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
--- src/main/java/betterwithmods/module/tweaks/MushroomFarming.java
+++ src/main/java/betterwithmods/module/tweaks/MushroomFarming.java
@@ -1,30 +1,49 @@
 package betterwithmods.module.tweaks;
 
+import betterwithmods.common.BWMBlocks;
+import betterwithmods.common.blocks.BlockBeacon;
+import betterwithmods.common.blocks.BlockMushroom;
 import betterwithmods.module.Feature;
+import betterwithmods.module.hardcore.beacons.CapabilityBeacon;
+import betterwithmods.module.hardcore.beacons.EnderchestCap;
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockMushroom;
+import net.minecraft.block.BlockDirt;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.common.capabilities.CapabilityManager;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import java.util.HashSet;
 
 public class MushroomFarming extends Feature {
-    private static int maxLightLevel;
-    private static boolean deleteShrooms;
-    private static HashSet<String> validMushrooms;
+    public static boolean ALIAS_MUSHROOMS;
+    public static int MAX_LIGHT_LEVEL;
+    public static HashSet<String> VALID_MUSHROOMS;
+
+    public static final Block RED_MUSHROOM = new BlockMushroom().setRegistryName("minecraft:red_mushroom");
+    public static final Block BROWN_MUSHROOM = new BlockMushroom().setRegistryName("minecraft:brown_mushroom");
 
     @Override
     public void setupConfig() {
-        maxLightLevel = loadPropInt("Maximum Light Level", "The highest lightlevel at which mushrooms will grow.", 0);
-        deleteShrooms = loadPropBool("Delete Mushrooms", "Whether mushrooms should be deleted instead of popping off.", true);
-        validMushrooms = loadPropStringHashSet("Valid Mushrooms","Registry names of affected mushrooms",new String[]{"minecraft:brown_mushroom","minecraft:red_mushroom"});
+        MAX_LIGHT_LEVEL = loadPropInt("Maximum Light Level", "The highest lightlevel at which mushrooms will grow.", 0);
+        VALID_MUSHROOMS = loadPropStringHashSet("Valid Other Mushrooms","Registry names of affected mushrooms other than vanilla ones.",new String[]{});
+        ALIAS_MUSHROOMS = loadPropBool("Alias Mushrooms","Aliases vanilla mushrooms to truly prevent them from growing. Turn this off if it causes conflicts.",true);
     }
+
+    @Override
+    public void preInit(FMLPreInitializationEvent event) {
+        if(ALIAS_MUSHROOMS) {
+            BWMBlocks.registerBlock(RED_MUSHROOM);
+            BWMBlocks.registerBlock(BROWN_MUSHROOM);
+        }
+    }
+
 
     @Override
     public void init(FMLInitializationEvent event) {
@@ -48,7 +67,7 @@ public class MushroomFarming extends Feature {
         BlockPos pos = event.getPos();
         IBlockState state = event.getPlacedBlock();
 
-        if(!world.isRemote && isMushroom(state) && !canGrowMushroom(world,pos))
+        if(isMushroom(state) && !canGrowMushroom(world,pos))
         {
             event.setCanceled(true);
         }
@@ -69,7 +88,8 @@ public class MushroomFarming extends Feature {
 
     private boolean isMushroom(IBlockState state)
     {
-        return validMushrooms.contains(state.getBlock().getRegistryName().toString());
+        Block block = state.getBlock();
+        return block.getRegistryName() != null && VALID_MUSHROOMS.contains(block.getRegistryName().toString());
     }
 
     private void popOffMushroom(World world, BlockPos pos)
@@ -77,8 +97,7 @@ public class MushroomFarming extends Feature {
         IBlockState state = world.getBlockState(pos);
         if(isMushroom(state) && !canGrowMushroom(world,pos))
         {
-            if(!deleteShrooms)
-                state.getBlock().dropBlockAsItem(world, pos, state, 0);
+            state.getBlock().dropBlockAsItem(world, pos, state, 0);
             world.setBlockToAir(pos);
         }
     }
@@ -86,7 +105,17 @@ public class MushroomFarming extends Feature {
     private boolean canGrowMushroom(World world,BlockPos pos)
     {
         int light = world.getLight(pos);
+        IBlockState soil = world.getBlockState(pos.down());
 
-        return light <= maxLightLevel;
+        return light <= MAX_LIGHT_LEVEL || isMushroomSoil(soil);
+    }
+
+    public static boolean isMushroomSoil(IBlockState state)
+    {
+        if (state.getBlock() == Blocks.MYCELIUM)
+            return true;
+        else if (state.getBlock() == Blocks.DIRT && state.getValue(BlockDirt.VARIANT) == BlockDirt.DirtType.PODZOL)
+            return true;
+        return false;
     }
 }
