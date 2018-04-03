a95dd3c24c42df116fe38aa0160de554526ad3fd
primetoxinz <primetoxinzz@gmail.com>
2018-04-03 18:35:19 -0400

    Make sure Turntable Recipe is save, will fix #837 for sure.

--- src/main/java/betterwithmods/common/registry/block/recipe/TurntableRecipe.java
+++ src/main/java/betterwithmods/common/registry/block/recipe/TurntableRecipe.java
@@ -5,6 +5,7 @@ import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by primetoxinz on 4/20/17.
@@ -31,30 +32,6 @@ public class TurntableRecipe extends BlockRecipe {
     }
 
     public IBlockState getProductState() {
-        if (productState == null)
-            return Blocks.AIR.getDefaultState();
-        return productState;
+        return Optional.ofNullable(productState).orElse(Blocks.AIR.getDefaultState());
     }
-
-    //    private Block result;
-//    private int resultMeta;
-//    public TurntableRecipe(ItemStack inputBlock, ItemStack outputBlock, List<ItemStack> scraps) {
-//        this(((ItemBlock)inputBlock.getItem()).getBlock(), inputBlock.getMetadata(),((ItemBlock)outputBlock.getItem()).getBlock(), outputBlock.getMetadata(),scraps);
-//    }
-//    public TurntableRecipe(Block block, int meta, Block result, int resultMeta, List<ItemStack> scraps) {
-//        super(block, meta, scraps);
-//        this.result = result;
-//        this.resultMeta = resultMeta;
-//    }
-//
-//    public ItemStack getResult() {
-//        if (result == null)
-//            return ItemStack.EMPTY;
-//        return new ItemStack(result, 1, resultMeta);
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return String.format("%s-> %s + [%s]", getStack(), getResult(), getOutputs());
-//    }
 }
