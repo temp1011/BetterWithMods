93c5a0ca3b411b29ac7b9df26e8f0d1cb6ce9c1f
primetoxinz <primetoxinzz@gmail.com>
2018-04-04 17:42:56 -0400

    Remove MiniTypeIngredient, it's not longer necessary

--- src/main/java/betterwithmods/module/gameplay/miniblocks/MiniBlockIngredient.java
+++ src/main/java/betterwithmods/module/gameplay/miniblocks/MiniBlockIngredient.java
@@ -3,17 +3,23 @@ package betterwithmods.module.gameplay.miniblocks;
 import betterwithmods.common.BWMRecipes;
 import betterwithmods.common.registry.block.recipe.BlockIngredient;
 import betterwithmods.module.gameplay.miniblocks.blocks.BlockMini;
+import com.google.gson.JsonObject;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.JsonUtils;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.common.crafting.CraftingHelper;
+import net.minecraftforge.common.crafting.IIngredientFactory;
+import net.minecraftforge.common.crafting.JsonContext;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MiniBlockIngredient extends BlockIngredient {
 
@@ -65,12 +71,28 @@ public class MiniBlockIngredient extends BlockIngredient {
                 if (!stack.isEmpty() && stack.getItem() instanceof ItemBlock) {
                     IBlockState state = BWMRecipes.getStateFromStack(stack);
                     Material material = state.getMaterial();
-                    BlockMini blockMini = MiniBlocks.MINI_MATERIL_BLOCKS.get(type).get(material);
+                    BlockMini blockMini = MiniBlocks.MINI_MATERIAL_BLOCKS.get(type).get(material);
                     stacks.add(MiniBlocks.fromParent(blockMini, state));
                 }
             }
             cache = stacks.toArray(new ItemStack[stacks.size()]);
         }
         return cache;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("type:%s parent:%s", type.name(), Arrays.toString(base.getMatchingStacks()));
+    }
+
+    @SuppressWarnings("unused")
+    public static class Factory implements IIngredientFactory {
+        @Nonnull
+        @Override
+        public Ingredient parse(JsonContext context, JsonObject json) {
+            String type = JsonUtils.getString(json, "minitype");
+            Ingredient i = CraftingHelper.getIngredient(JsonUtils.getJsonObject(json, "baseIngredient"), context);
+            return new MiniBlockIngredient(type, i);
+        }
     }
 }
