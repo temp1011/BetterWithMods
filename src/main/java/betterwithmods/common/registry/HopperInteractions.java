7520096341cf3e10076cd6df238f34ef7bb0961e
BordListian <bordlistian@hotmail.de>
2018-04-20 15:25:30 +0200

    Static

--- src/main/java/betterwithmods/common/registry/HopperInteractions.java
+++ src/main/java/betterwithmods/common/registry/HopperInteractions.java
@@ -31,38 +31,38 @@ public class HopperInteractions {
         RECIPES.add(recipe);
     }
 
-    public boolean remove(List<ItemStack> outputs,List<ItemStack> secondary) {
+    public static boolean remove(List<ItemStack> outputs,List<ItemStack> secondary) {
         return RECIPES.removeAll(findRecipe(outputs,secondary));
     }
 
-    public boolean removeFuzzy(List<ItemStack> outputs,List<ItemStack> secondary) {
+    public static boolean removeFuzzy(List<ItemStack> outputs,List<ItemStack> secondary) {
         return RECIPES.removeAll(findRecipeFuzzy(outputs,secondary));
     }
 
-    public boolean removeExact(List<ItemStack> outputs,List<ItemStack> secondary) {
+    public static boolean removeExact(List<ItemStack> outputs,List<ItemStack> secondary) {
         return RECIPES.removeAll(findRecipeExact(outputs,secondary));
     }
 
-    public boolean removeByInput(ItemStack input) {
+    public static boolean removeByInput(ItemStack input) {
         return RECIPES.removeAll(findRecipeByInput(input));
     }
 
-    protected List<HopperRecipe> findRecipe(List<ItemStack> outputs,List<ItemStack> secondary) {
+    protected static List<HopperRecipe> findRecipe(List<ItemStack> outputs,List<ItemStack> secondary) {
         List<HopperRecipe> recipes = findRecipeExact(outputs,secondary);
         if(recipes.isEmpty())
             recipes = findRecipeFuzzy(outputs,secondary);
         return recipes;
     }
 
-    protected List<HopperRecipe> findRecipeFuzzy(List<ItemStack> outputs,List<ItemStack> secondary) {
+    protected static List<HopperRecipe> findRecipeFuzzy(List<ItemStack> outputs,List<ItemStack> secondary) {
         return RECIPES.stream().filter(r -> InvUtils.matches(r.getOutputs(),outputs) && InvUtils.matches(r.getOutputs(),secondary)).collect(Collectors.toList());
     }
 
-    protected List<HopperRecipe> findRecipeExact(List<ItemStack> outputs,List<ItemStack> secondary) {
+    protected static List<HopperRecipe> findRecipeExact(List<ItemStack> outputs,List<ItemStack> secondary) {
         return RECIPES.stream().filter(r -> InvUtils.matchesExact(r.getOutputs(),outputs) && InvUtils.matchesExact(r.getSecondaryOutputs(),secondary)).collect(Collectors.toList());
     }
 
-    protected List<HopperRecipe> findRecipeByInput(ItemStack input) {
+    protected static List<HopperRecipe> findRecipeByInput(ItemStack input) {
         return RECIPES.stream().filter(r -> r.input.apply(input)).collect(Collectors.toList());
     }
 
