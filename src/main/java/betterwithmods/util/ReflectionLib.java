a0f03acc5c11cf6f1f8850e990e4c456afc6609a
primetoxinz <primetoxinzz@gmail.com>
2018-05-11 22:55:23 -0400

    Add ReflectionLib. Need to add all reflection to this class for easy porting.
    (cherry picked from commit b083552)

---
+++ src/main/java/betterwithmods/util/ReflectionLib.java
@@ -0,0 +1,12 @@
+package betterwithmods.util;
+
+
+import org.apache.commons.lang3.tuple.Pair;
+
+
+//TODO FIND ALL REFLECTION AND PUT THE PAIRS HERE
+public class ReflectionLib {
+
+    public static final Pair<String,String> ENTITY_PLAYER_SET_FLAG = Pair.of("setFlag","func_70052_a");
+
+}
